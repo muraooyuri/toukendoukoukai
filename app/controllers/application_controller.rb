5706664453b8ccf45a2512ef66dd653c68293dcc
muraooyuri <yuri.0112hm@icloud.com>
2023-02-25 12:22:26 +0000

    Admin遷移

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,13 @@ class ApplicationController < ActionController::Base
 before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    user_path(current_user)
+
+    case resource
+    when Admin
+      admin_toukens_path
+    when User
+      user_path(current_user)
+    end
   end
 
   def after_sign_out_path_for(resource)
